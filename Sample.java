00db4b6f76e9ce5b64112179ea5b59c6369a18ed
bhuvanesh1019 <bhuvaneshamsa@gmail.com>
2023-05-04 11:17:12 +0530

    bhuvan git

---
+++ Sample.java
@@ -0,0 +1,15 @@
+package sample.java;
+
+public class Sample {
+	public static void main(String[] args) {
+		int num=123;
+		int rev=0;
+		while(num>0) {
+			int n=num%10;
+			rev=(rev*10)+n;
+			num=num/10;
+		}
+		System.out.println(rev);
+	}
+
+}
